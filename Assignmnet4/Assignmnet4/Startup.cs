60346da6fbcd701875a33229f9baf41d7616a3ea
Sohaib Arif <sohaibarif470@gmail.com>
2018-10-06 16:04:23 +0500

    Front end Layout

---
+++ Assignmnet4/Assignmnet4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignmnet4.Startup))]
+namespace Assignmnet4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
