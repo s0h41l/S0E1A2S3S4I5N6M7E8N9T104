eaf65b574c410e0669e52c9bdf03181b0506053c
Sohail Khan <sohailkhan.sk@hotmail.com>
2018-10-07 13:15:44 -0700

    Contacts functionalities

---
+++ Assignmnet4/Assignmnet4/Alerts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignmnet4
+{
+    public class Alerts
+    {
+        public static string alert = null;
+    }
+}
